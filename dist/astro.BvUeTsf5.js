88f11a985bca8565a670b2e69deddb616b67ab86
xinnxz <luthfifahmi.alv@gmail.com>
2025-12-16 05:39:20 +0700

    fix: add cleanUrls to vercel.json for proper routing

---
+++ dist/astro.BvUeTsf5.js
@@ -0,0 +1,4 @@
+import "piccolore";
+import "html-escaper";
+import "clsx";
+import "./astro/server.ti5nVkq3.js";
---
+++ dist/panel-left.DupZSLtO.js
@@ -0,0 +1,6 @@
+import{b as createLucideIcon}from"./SafeIcon.C08LGL0m.js";/**
+ * @license lucide-react v0.545.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const __iconNode=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"M9 3v18",key:"fh3hqa"}]],PanelLeft=createLucideIcon("panel-left",__iconNode);export{PanelLeft as P};
