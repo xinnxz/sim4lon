1216b206a2110c43d498100df5a19e428e7512a9
xinnxz <luthfifahmi.alv@gmail.com>
2026-01-27 22:25:46 +0700

    edit demo

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -310,7 +310,7 @@ export default function LoginForm() {
 
                   {/* Password Info */}
                   <p className="text-xs text-gray-500 mt-3 pt-2 border-t border-gray-200">
-                    <span className="font-medium">Password:</span> Admin123 / Operator123 / Pangkalan123
+                    <span className="font-medium">Password:</span> Admin123@ / Operator123 / Pangkalan123
                   </p>
                 </div>
               </div>
